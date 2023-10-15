f4bc556202ae924c175e06766530719755436c44
MSK4477 <ks7997067@gmail.com>
2023-10-15 20:29:15 +0530

    completed

--- index.js
+++ index.js
@@ -4,21 +4,30 @@ import dbConnect from "./data/dbConnection/connectToDb.js";
 import cookieParser from "cookie-parser";
 import UserRouter from "./routes/userRouter.js";
 import productRouter from "./routes/productRoute.js";
+
+await dbConnect();
+
 const app = express();
-const PORT = 3000;
 
 app.use(cookieParser()); 
-app.use(express.json()); 
-app.use(cors());
-dbConnect();
-app.use("/user", UserRouter);
-app.use("/product", productRouter)
+app.use(express.json());
 
+
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true,
+}));
 
 app.get("/", (req, res) => {
-    res.status(200).json({ message: "hello world" });
+  res.status(200).json({ message: "hello world" });
 });
 
+app.use("/user", UserRouter);
+app.use("/product", productRouter);
+
+const PORT = 3000;
+
+
 app.listen(PORT, () => {
-    console.log("App listening on port", PORT);
+  console.log("App listening on port", PORT);
 });
